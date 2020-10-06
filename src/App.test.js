68d7a7d3f2d0c7c895c01ac4f2cf32895e4820f7
BenDunjay <bendunjay@outlook.com>
2020-10-06 14:53:45 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders a test", () => {});

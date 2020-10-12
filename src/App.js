f8721fb5223297f9e91b609eccc4777bc48960dc
BenDunjay <bendunjay@outlook.com>
2020-10-12 14:55:15 +0100

    bit of styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 
-function App() {
-  return (
-    <div className="App">
-      <Congrats />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1> Jotto </h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;

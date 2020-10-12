fb80c1103d6c43e9da065b2c493ccf34cf714953
BenDunjay <bendunjay@outlook.com>
2020-10-12 14:21:18 +0100

    update

--- src/App.test.js
+++ src/App.test.js
@@ -3,4 +3,6 @@ import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
-test("renders a test", () => {});
+test("renders a test", () => {
+    const wrapper = shallow(<App />)
+});
